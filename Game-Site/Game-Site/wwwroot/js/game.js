4ff49ebc3f1b5e5b0fd5e85b44de06a959dd1e5c
Ryan-Afzal <ryan.afzal@outlook.com>
2020-04-08 10:44:18 -0700

    Fixed the grid

---
+++ Game-Site/Game-Site/wwwroot/js/game.js
@@ -0,0 +1,54 @@
+﻿$('document').ready(function () {
+    var randomBool = function () {
+        return Math.random() >= 0.5;
+    }
+
+    var getNumberFromRowColumn = function (n, r, c) {
+        return (n * r) + c;
+    }
+
+    var createTile = function (color) {// color is bool; true = light; false = dark
+        var node = $("<div></div>").addClass("tile");
+
+        if (color) {
+            node.addClass("bg-light");
+        } else {
+            node.addClass("bg-dark");
+        }
+
+        return node;
+    }
+
+    var setTileColor = function (node, color) {
+        if (color) {
+            node.removeClass("bg-dark");
+            node.addClass("bg-light");
+        } else {
+            node.removeClass("bg-light");
+            node.addClass("bg-dark");
+        }
+    }
+
+    var swapTileColor = function (node) {
+        setTileColor(node, node.hasClass("bg-dark"));
+    }
+
+    var createRandomGrid = function (n) {
+        var body = $("#tile-grid-body");
+
+        for (i = 0; i < n; i++) {
+            var row = $("<tr></tr>");
+
+            for (j = 0; j < n; j++) {
+                var tile = createTile(randomBool()).attr('id', `tile-${getNumberFromRowColumn(n, i, j)}`);
+                var data = $("<td></td>").append(tile);
+
+                row.append(data);
+            }
+
+            body.append(row);
+        }
+    }
+
+    createRandomGrid(6);
+});
