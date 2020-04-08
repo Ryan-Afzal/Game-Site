4329e7d1438a6df60316d417b3af6fdcb95bace1
Ryan-Afzal <ryan.afzal@outlook.com>
2020-04-08 12:01:42 -0700

    Finished the basic Grid Game

--- Game-Site/Game-Site/wwwroot/js/game.js
+++ Game-Site/Game-Site/wwwroot/js/game.js
@@ -1,4 +1,14 @@
 ﻿$('document').ready(function () {
+    var level = {
+        active: true,
+        set: new Set()
+    };
+
+    var win = function () {
+        level.active = false;
+        $('#win-alert').show();
+    }
+
     var randomBool = function () {
         return Math.random() >= 0.5;
     }
@@ -7,13 +17,18 @@
         return (n * r) + c;
     }
 
-    var createTile = function (color) {// color is bool; true = light; false = dark
-        var node = $("<div></div>").addClass("tile");
+    var createTile = function (n, r, c, color) {// color is bool; true = light; false = dark
+        var node = $("<div></div>")
+            .addClass("tile")
+            .attr('id', `tile-${getNumberFromRowColumn(n, r, c)}`)
+            .attr('tile-r', r)
+            .attr('tile-c', c);;
 
         if (color) {
             node.addClass("bg-light");
         } else {
             node.addClass("bg-dark");
+            level.set.add(node.attr('id'));
         }
 
         return node;
@@ -23,9 +38,11 @@
         if (color) {
             node.removeClass("bg-dark");
             node.addClass("bg-light");
+            level.set.delete(node.attr('id'));
         } else {
             node.removeClass("bg-light");
             node.addClass("bg-dark");
+            level.set.add(node.attr('id'));
         }
     }
 
@@ -33,22 +50,62 @@
         setTileColor(node, node.hasClass("bg-dark"));
     }
 
+    var swapRowColColors = function (n, r, c) {
+        if (level.active) {
+            for (i = 0; i < c; i++) {
+                swapTileColor($(`#tile-${getNumberFromRowColumn(n, r, i)}`));
+            }
+
+            for (i = c + 1; i < n; i++) {
+                swapTileColor($(`#tile-${getNumberFromRowColumn(n, r, i)}`));
+            }
+
+            for (j = 0; j < r; j++) {
+                swapTileColor($(`#tile-${getNumberFromRowColumn(n, j, c)}`));
+            }
+
+            for (j = r + 1; j < n; j++) {
+                swapTileColor($(`#tile-${getNumberFromRowColumn(n, j, c)}`));
+            }
+
+            swapTileColor($(`#tile-${getNumberFromRowColumn(n, r, c)}`));
+        }
+    }
+
     var createRandomGrid = function (n) {
         var body = $("#tile-grid-body");
 
-        for (i = 0; i < n; i++) {
+        for (r = 0; r < n; r++) {
             var row = $("<tr></tr>");
 
-            for (j = 0; j < n; j++) {
-                var tile = createTile(randomBool()).attr('id', `tile-${getNumberFromRowColumn(n, i, j)}`);
+            for (c = 0; c < n; c++) {
+                var tile = createTile(n, r, c, randomBool());
                 var data = $("<td></td>").append(tile);
+
+                tile.click(function () {
+                    swapRowColColors(
+                        n,
+                        parseInt(this.getAttribute('tile-r')),
+                        parseInt(this.getAttribute('tile-c'))
+                    );
+
+                    if (level.set.size == 0) {
+                        win();
+                    }
+                });
 
                 row.append(data);
             }
 
             body.append(row);
         }
+
+        if (level.set.size == 0) {
+            console.log("empty grid, regenerating...");
+            createRandomGrid(n);
+        }
     }
 
-    createRandomGrid(6);
+    $('#win-alert').hide();
+    createRandomGrid(2);
 });
