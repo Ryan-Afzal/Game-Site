9d722d3f2aad151598078128123f2e2584b58107
Ryan-Afzal <ryan.afzal@outlook.com>
2020-04-14 14:34:52 -0700

    Added visual improvements
    Added:
    - Moves counter
    - Timer
    - Score display
    - Data display
    - Score calculation
    - auto-solver/min moves

--- Game-Site/Game-Site/wwwroot/js/game.js
+++ Game-Site/Game-Site/wwwroot/js/game.js
@@ -1,12 +1,88 @@
 ﻿$('document').ready(function () {
-    var level = {
-        active: true,
-        set: new Set()
-    };
+    var global_active = false;//Is game active (boolean)
+    var global_set = new Set();//Set of black tiles (JS Set)
+    var global_n = 0;//Board side length (int)
+    var global_minMoves = 0;//Minimum moves required to complete game (int)
+    var global_moves = 0;//Moves taken to complete game (int)
+    var global_startTime = null;//Start 'Date' in milliseconds since 1970 (int)
+    var global_endTime = null;//End 'Date' to complete game in milliseconds since 1970 (int)
+    var global_interval = null;//Timer interval (JS object)
+    var global_game_moves = null;//Moves label (jQuery object)
+    var global_game_timer = null;//Timer label (jQuery object)
 
     var win = function () {
-        level.active = false;
-        $('#win-alert').show();
+        global_active = false;
+        stopTimer();
+
+        var body = $("<div></div>")
+            .addClass("card-body")
+            .append($("<h3></h3>")
+                .addClass("card-title text-success text-center")
+                .text("You win!")
+            )
+            .append($("<p></p>")
+                .addClass("card-text text-center")
+                .text(`Score: ${score(global_n, global_moves, global_endTime - global_startTime)}`)
+            );
+
+        var card = $("<div></div>")
+            .addClass("card")
+            .append(body);
+
+        $('#win-alert').append(card);
+    }
+
+    var move = function () {
+        global_moves++;
+        global_game_moves.text(global_moves);
+    }
+
+    var startTimer = function () {
+        global_startTime = Date.now();
+        clearInterval(global_interval);
+        interval = setInterval(updateTimer, 1);
+    }
+
+    var stopTimer = function () {
+        clearInterval(interval);
+        global_endTime = Date.now();
+    }
+
+    var updateTimer = function () {
+        global_game_timer.text(timeFromMillis(global_startTime));
+    }
+
+    var timeFromMillis = function (startTime) {
+        var currentTime = Date.now();
+        var time = currentTime - startTime;
+
+        var hours = Math.floor(time / 3600000);
+        time %= 3600000;
+        var minutes = Math.floor(time / 60000);
+        time %= 60000;
+        var seconds = Math.floor(time / 1000);
+        time %= 1000;
+        var milliseconds = time;
+
+        return `${leftPad(hours, 2)}:${leftPad(minutes, 2)}:${leftPad(seconds, 2)}:${leftPad(milliseconds, 3)}`;
+    }
+
+    var leftPad = function (num, length) {
+        var str = num.toString();
+
+        var d = length - str.length;
+
+        if (d > 0) {
+            for (i = 0; i < d; i++) {
+                str = '0' + str;
+            }
+        }
+
+        return str;
+    }
+
+    var score = function (n, moves, time) {
+        return Math.floor((100 * Math.pow(n, 2) * global_minMoves) / ((time / 1000) * moves));
     }
 
     var randomBool = function () {
@@ -25,10 +101,10 @@
             .attr('tile-c', c);;
 
         if (color) {
-            node.addClass("bg-light");
-        } else {
             node.addClass("bg-dark");
-            level.set.add(node.attr('id'));
+            global_set.add(node.attr('id'));
+        } else {
+            node.addClass("bg-light");
         }
 
         return node;
@@ -38,11 +114,11 @@
         if (color) {
             node.removeClass("bg-dark");
             node.addClass("bg-light");
-            level.set.delete(node.attr('id'));
+            global_set.delete(node.attr('id'));
         } else {
             node.removeClass("bg-light");
             node.addClass("bg-dark");
-            level.set.add(node.attr('id'));
+            global_set.add(node.attr('id'));
         }
     }
 
@@ -51,7 +127,7 @@
     }
 
     var swapRowColColors = function (n, r, c) {
-        if (level.active) {
+        if (global_active) {
             for (i = 0; i < c; i++) {
                 swapTileColor($(`#tile-${getNumberFromRowColumn(n, r, i)}`));
             }
@@ -69,10 +145,14 @@
             }
 
             swapTileColor($(`#tile-${getNumberFromRowColumn(n, r, c)}`));
+
+            move();
         }
     }
 
     var createRandomGrid = function (n) {
+        var arr = [];
+
         var body = $("#tile-grid-body");
         body.empty();
 
@@ -80,7 +160,10 @@
             var row = $("<tr></tr>");
 
             for (c = 0; c < n; c++) {
-                var tile = createTile(n, r, c, randomBool());
+                var rand = randomBool();
+                arr.push([getNumberFromRowColumn(n, r, c), rand ? 1 : 0]);
+
+                var tile = createTile(n, r, c, rand);
                 var data = $("<td></td>").append(tile);
 
                 tile.click(function () {
@@ -90,7 +173,7 @@
                         parseInt(this.getAttribute('tile-c'))
                     );
 
-                    if (level.set.size == 0) {
+                    if (global_active && global_set.size == 0) {
                         win();
                     }
                 });
@@ -101,32 +184,80 @@
             body.append(row);
         }
 
-        if (level.set.size == 0) {
+        if (global_set.size == 0) {
             console.log("empty grid, regenerating...");
             createRandomGrid(n);
         }
+
+        global_active = true;
+        global_minMoves = solveGame(n, arr);
     }
 
-    $('#win-alert').hide();
+    var setupGame = function (n, color) {
+        createRandomGrid(n);
+
+        global_n = n;
+        global_game_moves = $('#game-moves').text(0);
+        $('#game-size')
+            .addClass(`badge-${color}`)
+            .text(`${n}\u00D7${n}`);
+        global_game_timer = $('#game-timer');
+        startTimer();
+    }
+
+    var solveGame = function (n, arr) {// Returns minimum moves required to solve given board
+        //Start with two arrays: rows and columns
+        var r = [];
+        var c = [];
+
+        //Start with them all at 0
+        for (i = 0; i < n; i++) {
+            r[i] = 0;
+            c[i] = 0;
+        }
+
+        // Set each index in the rows and columns to be the number of black tiles in each row/column
+        for (_r = 0; _r < n; _r++) {
+            for (_c = 0; _c < n; _c++) {
+                if (arr[getNumberFromRowColumn(n, _r, _c)][1] == 1) {
+                    r[_r]++;
+                    c[_c]++;
+                }
+            }
+        }
+
+        //A move is required for each tile in the [i, j] grid where r[i] + c[j] + arr[i][j] is odd
+        var moves = 0;
+        for (_r = 0; _r < n; _r++) {
+            for (_c = 0; _c < n; _c++) {
+                var i = r[_r] + c[_c] + arr[getNumberFromRowColumn(n, _r, _c)][1];
+                if (i % 2 == 1) {
+                    moves++;
+                }
+            }
+        }
+
+        return moves;
+    }
 
     $('#difficulty-2x2').click(function () {
-        createRandomGrid(2);
+        setupGame(2, "success");
     });
 
     $('#difficulty-4x4').click(function () {
-        createRandomGrid(4);
+        setupGame(4, "info");
     });
 
     $('#difficulty-6x6').click(function () {
-        createRandomGrid(6);
+        setupGame(6, "primary");
     });
 
     $('#difficulty-8x8').click(function () {
-        createRandomGrid(8);
+        setupGame(8, "warning");
     });
 
     $('#difficulty-10x10').click(function () {
-        createRandomGrid(10);
+        setupGame(10, "danger");
     });
 
     $('#difficultySelect').modal('show');
