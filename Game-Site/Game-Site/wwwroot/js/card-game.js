ef59d413125930a023eb2e4a049f1e0db62a14d7
Ryan-Afzal <ryan.afzal@outlook.com>
2020-05-02 17:05:26 -0700

    Worked on the card game

--- Game-Site/Game-Site/wwwroot/js/card-game.js
+++ Game-Site/Game-Site/wwwroot/js/card-game.js
@@ -7,37 +7,54 @@ $('document').ready(function () {
     var cards = [];
     var selectedCards = [];
 
-    var n = 15;
+    var n = 18;
     var k = 4;
 
     // Create card #n.
-    var createCard = function (n) {
-        throw new DOMException();
+    var createCard = function (n, width, height) {
+        //throw new DOMException();
+        var node = $("<div></div>")
+            .attr("id", `card-${n}`)
+            .attr("card-num", Math.floor(n / 2) + 1)
+            .addClass("game-card bg-dark")
+            .attr("style", `width: ${width}px; height: ${height}px;`);
+
+        return node;
     }
 
     // Populates the card grid with n cards
     var createCardDeck = function () {
-        var cards = [];
-        var selectedCards = [];
+        cards = [];
+        selectedCards = [];
 
         var column = $("#card-grid-col");
-
+        var grid = $("#card-grid");
         var body = $("#card-grid-body");
         body.empty();
 
-        var numCards = n * 2;
-        var rows = 5;
+        var rows = 3;
         var cols = 6;
 
         var i = 0;
+
+        var width = column.width();
+        var spacing = grid.attr('cellspacing');
+        var padding = grid.attr('cellpadding');
+
+        var width = (width - (padding * 2 * cols) - (spacing * (cols - 1))) / cols;
+        var height = width * 3 / 2;
 
         for (var r = 0; r < rows; r++) {
             var row = $("<tr></tr>");
 
             for (var c = 0; c < cols; c++) {
-                var card = createCard(i);
+                var card = createCard(i, width, height);
 
-                cards.append(card);
+                card.click(function () {
+
+                });
+
+                cards.push(card);
 
                 row.append(
                     $("<td></td>")
@@ -53,6 +70,22 @@ $('document').ready(function () {
 
     var shuffle = function (cards) {
         throw new DOMException();
+    }
+
+    var selectCard = function (card) {// Select a card
+
+    }
+
+    var turnOverCards = function () {// Turns over selected k cards.
+
+    }
+
+    var shuffleCards = function () {// Turns selected cards over, deselects them, and shuffles them.
+
+    }
+
+    var win = function () {
+
     }
 
     createCardDeck();
