dc5f0ce58b55644272ebb8ad54ad0b7b34150747
cosinechicken <ah.blenden@gmail.com>
2020-11-06 23:55:52 -0800

    Started writing linalg.ts
    Started work converting Math.java from LinAlg repository to linalg.ts

--- Game-Site/Game-Site/Scripts/linalg.ts
+++ Game-Site/Game-Site/Scripts/linalg.ts
@@ -1,1 +1,34 @@
-﻿console.log("hi");
+﻿class Numbers {
+    abs(n) {
+        if (n > 0) {
+            return n;
+        } else {
+            return -n;
+        }
+    }
+    // Finds gcd of i and j (want my own implementation to make sure negatives are dealt with correctly and to show off)
+    gcd(i, j) {
+        // Take absolute values
+        let iPos = this.abs(i);
+        let jPos = this.abs(j);
+        if (iPos == jPos && iPos == 0) {
+            return 0;
+        } else if (iPos == 0) {
+            return jPos;
+        } else if (jPos == 0) {
+            return iPos;
+        } else if (iPos > jPos) {
+            return this.gcd(iPos % jPos, jPos);
+        } else {
+            return this.gcd(iPos, jPos % iPos);
+        }
+    }
+    // Finds gcd of integers in an integer array arr
+    gcdArr(arr) {
+        let gcd = this.abs(arr[0]);
+        for (let i of gcd) {
+            gcd = this.gcd(gcd, i);
+        }
+        return gcd;
+    }
+}
