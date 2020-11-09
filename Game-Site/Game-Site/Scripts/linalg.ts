6f8255194ec6f12c15e27dd54c8b1b897dfa1da2
cosinechicken <ah.blenden@gmail.com>
2020-11-08 23:39:20 -0800

    Implemented more methods
    More methods implemented from Matrix.java (other than toRREF()).

--- Game-Site/Game-Site/Scripts/linalg.ts
+++ Game-Site/Game-Site/Scripts/linalg.ts
@@ -41,7 +41,7 @@ class Numbers {
         return false;
     }
     // Returns the smaller of the two numbers.
-    min(i: number, j: number): number {
+    static min(i: number, j: number): number {
         if (i < j) {
             return i;
         } else {
@@ -92,7 +92,7 @@ class Fraction {
             this.D *= -1;
         }
     }
-    public toString(): String {
+    public toString(): string {
         this.simplify();
         if (this.D == 1) {
             return this.N + "";
@@ -132,5 +132,200 @@ class Matrix {
         if (!isNonZero) {
             this.pivots[i] = this.N;
         }
+    }
+    public toString(): string {
+        let str: string = "";
+        for (let i = 0; i < this.M; i++) {
+            for (let j = 0; j < this.N; j++) {
+                str += this.vals[i][j];
+                str += "\t";
+            }
+            str += "\n";
+        }
+        return str;
+    }
+    // Swap rows i and j (1-based indices)
+    // Prints if print == true
+    swap(i: number, j: number, print: boolean): string {
+        let temp: number;
+        for (let k = 0; k < this.N; k++) {
+            temp = this.vals[i][k];
+            this.vals[i][k] = this.vals[j][k];
+            this.vals[j][k] = temp;
+        }
+        temp = this.pivots[i];
+        this.pivots[i] = this.pivots[j];
+        this.pivots[j] = temp;
+        if (print) {
+            return ("R_" + (i + 1) + " <-> R_" + (j + 1));
+        } else {
+            return "";
+        }
+    }
+    // Scale values in row i by some integer scale, returns the scale
+    scale(i: number, scale: number): number {
+        for (let j = 0; j < this.N; j++) {
+            this.vals[i][j] *= scale;
+        }
+        if (scale == 0) {
+            this.pivots[i] = this.N;
+        }
+        return scale;
+    }
+    // Add row i to row j
+    addRow(i: number, j: number) {
+        for (let k = 0; k < this.N; k++) {
+            this.vals[j][k] += this.vals[i][k];
+        }
+        // Find pivot (inefficient)
+        let isNonZero: boolean = false;
+        for (let k = 0; k < this.N; k++) {
+            this.vals[j][k] = this.vals[j][k];
+            if (this.vals[j][k] != 0 && !isNonZero) {
+                isNonZero = true;
+                this.pivots[j] = k;
+            }
+        }
+        if (!isNonZero) {
+            this.pivots[j] = this.N;
+        }
+    }
+    // Divides by gcd of numbers in row i and makes the first nonzero entry positive
+    descale(i: number): number {
+        let gcd = Numbers.gcdArr(this.vals[i]);
+        let factor = gcd;
+        if (gcd != 0) {
+            for (let j = 0; j < this.N; j++) {
+                this.vals[i][j] /= gcd;
+            }
+            // Make leftmost nonzero variable positive
+            // Since gcd != 0 some variable is nonzero
+
+            if (this.vals[i][this.pivots[i]] < 0) {
+                factor *= -1;
+                for (let j = 0; j < this.N; j++) {
+                    this.vals[i][j] *= -1;
+                }
+            }
+        } else {
+            factor = 1;
+        }
+        return factor;
+    }
+    // Adds a scalar multiple of row i to row j so that vals[j][pivots[i]] = 0 (or vice versa)
+    // Also helper method for toREF()
+    // Prints the string it is supposed to print if print is true
+    cancel(i: number, j: number, print: boolean): [Fraction, string] {
+        // (a,b) = (i,j) if pivots[i] > pivots[j] or pivots[i] = pivots[j] and i < j, and (j,i) otherwise
+        let a: number;
+        let b: number;
+        let str: string = "";
+        let multiplier: Fraction = new Fraction(1, 1);
+        if (this.pivots[i] > this.pivots[j] || (this.pivots[i] == this.pivots[j] && i < j)) {
+            a = i;
+            b = j;
+        } else {
+            a = j;
+            b = i;
+        }
+        // Now begin cancelling
+        // First step is scale down both rows
+        let aScale: number = this.descale(a);
+        if (aScale != 1) {
+            let aScaleFrac: Fraction = new Fraction(1, aScale);
+            multiplier.multiply(aScale);
+            if (print) {
+                str += ("R_" + (a + 1) + " -> (" + aScaleFrac + ") * R_" + (a + 1) + "\n");
+                str += (this.toString() + "\n");
+            }
+        }
+        let bScale: number = this.descale(b);
+        if (bScale != 1) {
+            let bScaleFrac: Fraction = new Fraction(1, bScale);
+            multiplier.multiply(bScale);
+            if (print) {
+                str += ("R_" + (b + 1) + " -> (" + bScaleFrac + ") * R_" + (b + 1) + "\n");
+                str += (this.toString() + "\n");
+            }
+        }
+
+        // Fraction written in form scaleA_N/scaleA_D
+        let scaleA_N: number = 1;
+        let scaleA_D: number = 1;
+        scaleA_N *= this.vals[b][this.pivots[a]];
+        scaleA_D *= -this.vals[a][this.pivots[a]];
+
+        this.scale(a, scaleA_N);
+        this.scale(b, scaleA_D);
+        multiplier.divide(scaleA_N);
+        multiplier.divide(scaleA_D);
+        this.addRow(a, b);
+        multiplier.multiply(this.descale(a));
+        let scaleB_D: number = this.descale(b);
+        multiplier.multiply(scaleB_D);
+
+        let scaleAFrac: Fraction = new Fraction(scaleA_N, scaleB_D);
+        let scaleBFrac: Fraction = new Fraction(scaleA_D, scaleB_D);
+
+        if (print) {
+            str += ("R_" + (b + 1) + " -> (" + scaleBFrac + ") * R_" + (b + 1) + " + (" + scaleAFrac + ") * R_" + (a + 1));
+        }
+        multiplier.simplify();
+        return [multiplier,str];
+    }
+    // Helper methods
+
+
+    // REF stands for Row Echelon Form
+    // print determines whether to print the steps
+    toREF(print: boolean): [Fraction, string] {
+        let str: string = "";
+        let multiplier: Fraction = new Fraction(1, 1);
+        // First find rows with minimum value for pivot
+        let arr: number[] = [];
+        for (let i = 0; i < this.M; i++) {
+            let pivotVal: number = Number.MAX_VALUE;
+            let firstIndex: number = -1;
+            arr = [];
+            let arrSize: number = 0;
+            for (let j = i; j < this.M; j++) {
+                if (this.pivots[j] < pivotVal) {
+                    pivotVal = this.pivots[j];
+                    arr = [];
+                    arrSize = 0;
+                    firstIndex = j;
+                } else if (this.pivots[j] == pivotVal) {
+                    arr.push(j);
+                    arrSize++;
+                }
+            }
+            if (pivotVal < this.N) {
+                if (firstIndex != i) {
+                    this.swap(i, firstIndex, print);
+                    multiplier.multiply(-1);
+                    if (print) {
+                        str += "\n";
+                        str += (this.toString() + "\n");
+                    }
+                }
+                // Now we can assume the index we want to apply this.cancel() with is at i
+                for (let j = 0; j < arrSize; j++) {
+                    let cancelRet: [Fraction, string] = this.cancel(i, arr[j], print);
+                    multiplier.multiplyFraction(cancelRet[0]);
+                    if (print) {
+                        str += cancelRet[1];
+                        str += "\n";
+                        str += (this.toString() + "\n");
+                    }
+                    multiplier.simplify();
+                }
+            }
+        }
+        // At this point all nonzero rows should have distinct pivots
+        for (let i = 0; i < Numbers.min(this.M, this.N); i++) {
+            multiplier.multiply(this.vals[i][i]);
+        }
+        multiplier.simplify();
+        return [multiplier, str];
     }
 }
