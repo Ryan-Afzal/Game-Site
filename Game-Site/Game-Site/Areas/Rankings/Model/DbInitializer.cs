98bfe5a2400f43fc64b3449f6471ce449518c767
cosinechicken <ah.blenden@gmail.com>
2021-05-03 16:23:15 -0700

    Implemented DbInitializer
    Basically copied stuff from Chat-V2

---
+++ Game-Site/Game-Site/Areas/Rankings/Model/DbInitializer.cs
@@ -0,0 +1,16 @@
+﻿using Grid_Game.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Grid_Game.Areas.Rankings.Model
+{
+    public class DbInitializer
+    {
+        public static void Initialize(ApplicationDbContext db)
+        {
+            db.Database.EnsureCreated();
+        }
+    }
+}
--- Game-Site/Game-Site/Program.cs
+++ Game-Site/Game-Site/Program.cs
@@ -2,17 +2,36 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Grid_Game.Areas.Rankings.Model;
+using Grid_Game.Model;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Grid_Game {
 	public class Program {
 		public static void Main(string[] args) {
-			CreateHostBuilder(args)
-				.Build()
-				.Run();
+			var host = CreateHostBuilder(args).Build();
+
+			using (var scope = host.Services.CreateScope())
+			{
+				var services = scope.ServiceProvider;
+				try
+				{
+					var context = services.GetRequiredService<ApplicationDbContext>();
+
+					DbInitializer.Initialize(context);
+				}
+				catch (Exception ex)
+				{
+					var logger = services.GetRequiredService<ILogger<Program>>();
+					logger.LogError(ex, "An error occurred creating the DB.");
+				}
+			}
+
+			host.Run();
 		}
 
 		public static IHostBuilder CreateHostBuilder(string[] args) =>
